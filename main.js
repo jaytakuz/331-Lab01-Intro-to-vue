1e135a91b2598e391c724efbb14b6d2b9c16a06b
Watcharapong Wanna <jaytakuz@users.noreply.github.com>
2025-06-26 23:03:55 +0700

    add variants of ID and colors

--- main.js
+++ main.js
@@ -12,8 +12,13 @@ createApp({
 
         const details = ref(['50% cotton', '30% wool', '20% polyester'])
 
+        const variants = ref([
+            { id: 2234, color: 'green'},
+            { id: 2235, color: 'blue'}
+        ])
+
         return {
-            product, productDescription, image, camturl, inStock, inventory, onSale, details
+            product, productDescription, image, camturl, inStock, inventory, onSale, details, variants
         }
     }
 }).mount('#app')
