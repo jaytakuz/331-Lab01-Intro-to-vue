a8f5349640bd89c9de0b17e1026d4eb5d5e9a697
Watcharapong Wanna <jaytakuz@users.noreply.github.com>
2025-06-26 23:26:24 +0700

    add updateImage function with mouse hover

--- main.js
+++ main.js
@@ -13,8 +13,8 @@ createApp({
         const details = ref(['50% cotton', '30% wool', '20% polyester'])
 
         const variants = ref([
-            { id: 2234, color: 'green'},
-            { id: 2235, color: 'blue'}
+            { id: 2234, color: 'green', image: 'assets/images/socks_green.jpg'},
+            { id: 2235, color: 'blue', image: 'assets/images/socks_blue.jpg'}
         ])
 
         const sizes = ref(['S', 'M', 'L'])
@@ -24,8 +24,12 @@ createApp({
             cart.value +=1
         }
 
+        function updateImage(variantImage) {
+            image.value = variantImage
+        }
+
         return {
-            product, productDescription, image, camturl, inStock, inventory, onSale, details, variants, sizes, cart, addToCart
+            product, productDescription, image, camturl, inStock, inventory, onSale, details, variants, sizes, cart, addToCart, updateImage
         }
     }
 }).mount('#app')
